bb930dcf6e3c4a9150685d78c4e7cc5f60ac6ef7
JorgeAssaf <jorgeassaf160@gmail.com>
2023-03-10 12:35:15 -0600

    feat: 🎸 new api and new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,63 @@
+
+// export interface Noticia {
+//   source: Source;
+//   author: string;
+//   title: string;
+//   description: null;
+//   url: string;
+//   urlToImage: null;
+//   publishedAt: Date;
+//   content: null;
+// }
+
+// export interface Source {
+//   id: ID;
+//   name: Name;
+// }
+
+// export enum ID {
+//   GoogleNews = "google-news",
+// }
+
+// export enum Name {
+//   GoogleNews = "Google News",
+// }
+
+
 export interface Noticia {
-  source: Source;
-  author: string;
+  status: string;
+  totalResults: number;
+  results: Noticia[];
+  nextPage: string;
   title: string;
-  description: null;
-  url: string;
-  urlToImage: null;
-  publishedAt: Date;
-  content: null;
+  link: string;
+  keywords: string[] | null;
+  creator: string[] | null;
+  video_url: null;
+  description: string;
+  content: null | string;
+  pubDate: Date;
+  image_url: string;
+  source_id: SourceID;
+  category: Category[];
+  country: Country[];
+  language: Language;
 }
 
-export interface Source {
-  id: ID;
-  name: Name;
+export enum Category {
+  Health = "health",
 }
 
-export enum ID {
-  GoogleNews = "google-news",
+export enum Country {
+  Mexico = "mexico",
 }
 
-export enum Name {
-  GoogleNews = "Google News",
+export enum Language {
+  Spanish = "spanish",
+}
+
+export enum SourceID {
+  Diariocordoba = "diariocordoba",
+  Diariofarma = "diariofarma",
+  Redaccionmedica = "redaccionmedica",
 }
